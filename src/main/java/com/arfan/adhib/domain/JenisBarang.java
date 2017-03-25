bde52db8e0ca695b90191d4b15529044124e9d1c
Adhib Arfan <adhib.arfan@gmail.com>
2017-03-25 11:25:51 +0700

    add enum jenis barang

---
+++ src/main/java/com/arfan/adhib/domain/JenisBarang.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.arfan.adhib.domain;
+
+/**
+ *
+ * @author emerio
+ */
+public enum JenisBarang {
+
+    padat, cair, gas
+
+}
